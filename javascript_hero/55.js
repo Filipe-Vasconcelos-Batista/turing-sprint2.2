20bcc169b6dd3c8bc63d5906334508149bc60d1c
Filipe Batista <filipevbatista@live.com>
2023-10-04 14:03:25 +0200

    55 finished

---
+++ javascript_hero/55.js
@@ -0,0 +1,9 @@
+/*Sorting arrays
+Now we can sort arrays with given length.
+Exercise
+Write a function sort that takes an array filled with 3 numbers and returns these 3 numbers sorted in ascending order as an array.
+
+Example: sort([2, 3, 1]) should return [1, 2, 3].*/
+const sort= array =>array.sort()
+
+console.log(sorte([3,1,2]))
