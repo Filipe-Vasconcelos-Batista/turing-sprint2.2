6ffb12c82ae14af48571057f11b0906cc2127117
Filipe Batista <filipevbatista@live.com>
2023-10-05 13:38:00 +0200

    64 finished

--- javascript_hero/64.js
+++ javascript_hero/64.js
@@ -1,14 +1,31 @@
 /* null
-Besides undefined JavaScript knows another value for "no value": null. You can assign the value null to a variable and test for null with strict equality:
+Besides undefined JavaScript knows another value for "no value": 
+null. You can assign the value null to a variable and test for null
+ with strict equality:
 
 let foo = null;
 if (foo === null) {
   foo = 1;
 }
 
-It is a mistake that JavaScript has two values for "no value". One should always treat undefined and null equally.
+It is a mistake that JavaScript has two values for "no value". 
+One should always treat undefined and null equally.
 Exercise
-Write a function cutComment that takes one line of JavaScript and returns a possible line comment trimmed. If the line contains no line comment, null should be returned. For simplicity, we assume that the code does not contain the comment characters within a string.
+Write a function cutComment that takes one line of JavaScript and 
+returns a possible line comment trimmed. If the line contains no line
+comment, null should be returned. For simplicity, we assume that 
+the code does not 
+contain the comment characters within a string.
 
 Example: cutCommt('let foo; // bar') should return 'bar'.
 */
+const cutCommt = str =>{
+  if (str.search("//")=== -1){
+    return null
+  }
+  else{
+    //let x= str.search("//");
+    return str.substring(str.search("//")+3)
+  }
+}
+console.log(cutCommt('let foo; // bar'),cutCommt('let foo; bar'))
